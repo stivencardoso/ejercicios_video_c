8521ec9a300ed333ad8a978eebbcca7b7f37db91
mariana <jstiven927@gmail.com>
2023-08-04 15:57:18 -0500

    se agrega el proyecto

---
+++ simplecasses/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace simpleclasses
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            car mycar = new car();
+            mycar.make = "oldmobile";
+            mycar.model = "cutlas supreme";
+            mycar.year = 1986;
+            mycar.color = "silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", mycar.make, mycar.model, mycar.year, mycar.color);
+            //decimal value = determinemarketvalue(mycar);
+            //Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:c}", mycar.determinemarketvalue());
+            Console.ReadLine();
+        }
+        private static decimal determinemarketvalue(car car)
+        {
+            decimal carvalue = 100.0M;
+            return carvalue;
+        }
+    }
+    class car
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+
+        public decimal determinemarketvalue()
+        {
+            decimal carvalue;
+            if (year > 1990)
+                carvalue = 10000;
+            else
+                carvalue = 2000;
+
+            return carvalue;
+        }
+    }
+}
